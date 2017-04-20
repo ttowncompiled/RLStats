66666019d669a1aa0bfa64d1a27b97e178cd5e2d
Ian Riley <ian.riley.915@gmail.com>
2017-04-20 11:10:40 -0500

    Calculates average team utility.

--- utility-analysis.py
+++ utility-analysis.py
@@ -14,24 +14,125 @@ teams = None
 with open(season_teams_file_name, 'r') as f_in:
     teams = json.loads(f_in.read().strip())
 
+team_flow = {}
 for t, players in teams.iteritems():
     team_path_file = './Paths/%s-s_%d.pdat' % (t, season)
     if not os.path.exists(team_path_file):
         print('! ERROR: team path file does not exist (%s).' % t)
         exit(0)
     paths = 0
-    player_part = {}
+    player_flow = {}
     for p in players:
-        player_part[p] = 0
+        player_flow[p] = 0
     with open(team_path_file, 'r') as f:
         for line in f:
             line = line.strip()
             nodes = line.split(' ')[1:-1]
             N = {}
             for n in nodes:
-              N[n] = True
+                N[n] = True
             for n in N.keys():
-                if n in player_part:
-                    player_part[n] += 1
+                if n in player_flow:
+                    player_flow[n] += 1
             paths += 1
-    print(player_part)
+    for p in players:
+        player_flow[p] = round((player_flow[p] * 1.0) / paths, 3)
+    team_flow[t] = player_flow
+
+ranks = []
+for t, player_flow in team_flow.iteritems():
+    f = 0
+    for p, flow in player_flow.iteritems():
+        f += flow
+    f = round((f * 1.0) / 3, 3)
+    ranks.append((f, t))
+
+ranks.sort()
+ranks.reverse()
+
+pprint(ranks)
+
+team_flow = {}
+for t, players in teams.iteritems():
+    team_path_file = './Paths/%s-s_%d.pdat' % (t, season)
+    if not os.path.exists(team_path_file):
+        print('! ERROR: team path file does not exist (%s).' % t)
+        exit(0)
+    paths = 0
+    player_flow = {}
+    for p in players:
+        player_flow[p] = 0
+    with open(team_path_file, 'r') as f:
+        for line in f:
+            line = line.strip()
+            nodes = line.split(' ')[1:-1]
+            if len(nodes) > 3:
+                nodes = nodes[-3:]
+            N = {}
+            for n in nodes:
+                N[n] = True
+            for n in N.keys():
+                if n in player_flow:
+                    player_flow[n] += 1
+            paths += 1
+    for p in players:
+        player_flow[p] = round((player_flow[p] * 1.0) / paths, 3)
+    team_flow[t] = player_flow
+
+ranks = []
+for t, player_flow in team_flow.iteritems():
+    f = 0
+    for p, flow in player_flow.iteritems():
+        f += flow
+    f = round((f * 1.0) / 3, 3)
+    ranks.append((f, t))
+
+ranks.sort()
+ranks.reverse()
+
+pprint(ranks)
+
+team_flow = {}
+for t, players in teams.iteritems():
+    team_path_file = './Paths/%s-s_%d.pdat' % (t, season)
+    if not os.path.exists(team_path_file):
+        print('! ERROR: team path file does not exist (%s).' % t)
+        exit(0)
+    player_flow = {}
+    for p in players:
+        player_flow[p] = (0, 0, 0)
+    with open(team_path_file, 'r') as f:
+        for line in f:
+            line = line.strip()
+            nodes = line.split(' ')[1:-1]
+            if len(nodes) > 3:
+                nodes = nodes[-3:]
+            N = {}
+            for n in nodes:
+                N[n] = True
+            for n in N.keys():
+                if n in player_flow:
+                    flow = player_flow[n]
+                    if nodes[-1] == 'Score':
+                        flow = (flow[0] + 1, flow[1], 0)
+                    else:
+                        flow = (flow[0], flow[1] + 1, 0)
+                    player_flow[n] = flow
+    for p in players:
+        flow = player_flow[p]
+        flow = (flow[0], flow[1], round((flow[0] * 1.0) / (flow[0] + flow[1]), 3))
+        player_flow[p] = flow
+    team_flow[t] = player_flow
+
+ranks = []
+for t, player_flow in team_flow.iteritems():
+    f = 0
+    for p, flow in player_flow.iteritems():
+        f += flow[2]
+    f = round((f * 1.0) / 3, 3)
+    ranks.append((f, t))
+
+ranks.sort()
+ranks.reverse()
+
+pprint(ranks)
